61021c38b992e37861500dbc1382d68543d78018
plongg-210 <0000philong@gmail.com>
2026-01-12 16:15:51 +0700

    update

---
+++ Web_03_a/src/java/model/UserDAO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PHI LONG
+ */
+public class UserDAO {
+
+    ArrayList<UserDTO> list = new ArrayList<>();
+
+    public UserDAO() {
+        list.add(new UserDTO("admin", "admin", "Nguyen Van A"));
+        list.add(new UserDTO("user1", "user1", "Nguyen Van B"));
+
+    }
+    
+    public UserDTO searchById(String id){
+        for(UserDTO userDTO : list) {
+            if(userDTO.getUsername().equalsIgnoreCase(id)){
+                return userDTO;
+            }
+        }
+        return null;
+    }
+    
+    public UserDTO login(String username, String password){
+        UserDTO user = searchById(username);
+        if(user != null && user.getPassword().equals(password)){
+            return user;
+        }else{
+            return null;
+        }
+    }
+
+}
